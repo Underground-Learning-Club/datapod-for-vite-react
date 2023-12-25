1970459912ad16b663f91a6520e62d6f666f4b40
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-25 01:51:38 +0100

    showing default

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,28 +1,23 @@
-import { createContext} from "react";
+import { createContext, useState } from "react";
 import { IAppData, blankAppData } from "./interfaces";
 
 interface IAppContext {
-	appData: IAppData,
-	title: string;
+	appData: IAppData;
 }
 
 interface IAppProvider {
 	children: React.ReactNode;
 }
 
-const appData = blankAppData;
-const title = 'the title';
-
 export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
-	// const [appData] = useState<IAppData>(blankAppData);
-	
+	const [appData] = useState<IAppData>(blankAppData);
+
 	return (
 		<AppContext.Provider
 			value={{
-				appData,
-				title
+				appData
 			}}
 		>
 			{children}
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -2,15 +2,12 @@ import { useContext } from "react";
 import { AppContext } from "../AppContext";
 
 export const PageWelcome = () => {
-	const { appData, title } = useContext(AppContext);
-
-	console.log(appData);
+	const { appData } = useContext(AppContext);
 
 	return (
 		<>
 			<p>Welcome to this site.</p>
 			{appData && <p>App ID-Code: {appData.appIdCode}</p>}
-			<p>title: [{title}]</p>
 		</>
 	);
 };
