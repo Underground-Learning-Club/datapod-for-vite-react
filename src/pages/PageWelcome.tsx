f3355b38dc1f18a7f520ee8b5b51db3e86fb42d9
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-25 02:23:01 +0100

    showing all

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -6,23 +6,17 @@ export const PageWelcome = () => {
 
 	return (
 		<>
-			{appData && (
-				<>
-					<p>App ID-Code: {appData.appIdCode}</p>
-					<p>frontend port: {appData.frontendPort}</p>
-					<p>backend port: {appData.backendPort}</p>
-					<p>data files:</p>
-					<ul className="list-disc ml-6">
-						{appData.dataPathAndFileNames.map(
-							(dataPathAndFileName, index) => {
-								return (
-									<li key={index}>{dataPathAndFileName}</li>
-								);
-							}
-						)}
-					</ul>
-				</>
-			)}
+			<p>App ID-Code: {appData.appIdCode}</p>
+			<p>frontend port: {appData.frontendPort}</p>
+			<p>backend port: {appData.backendPort}</p>
+			<p>data files:</p>
+			<ul className="list-disc ml-6">
+				{appData.dataPathAndFileNames.map(
+					(dataPathAndFileName, index) => {
+						return <li key={index}>{dataPathAndFileName}</li>;
+					}
+				)}
+			</ul>
 		</>
 	);
 };
