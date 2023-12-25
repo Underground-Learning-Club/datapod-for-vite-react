ee438ec2071803c98ad6aa50479f8dcaf3f45504
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-25 11:11:13 +0100

    mock dpod loader

--- src/classes/DpodDataLoader.ts
+++ src/classes/DpodDataLoader.ts
@@ -1,13 +1,15 @@
+import * as qstr from '../../share/qtools/qstr';
+
 export class DpodDataLoader {
 	private content = '';
 	private lines: string[] = [];
 
 	constructor(content: string) {
 		this.content = content;
-		this.lines = [];
+		this.lines = qstr.convertStringBlockToLines(this.content);
 	}
 
-	getLines() {
-		return this.content
+	getNumberOfLines() {
+		return this.lines.length;
 	}
 }
