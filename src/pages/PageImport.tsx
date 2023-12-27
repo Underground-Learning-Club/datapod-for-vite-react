5b9aefedf2442cdf71aa04c009c6c9a52f85f85d
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-27 17:55:00 +0100

    showing files

--- src/pages/PageImport.tsx
+++ src/pages/PageImport.tsx
@@ -5,8 +5,24 @@ export const PageImport = () => {
 	const { appData } = useContext(AppContext);
 
 	return (
-		<>
-			<p>import page: {appData.appIdCode}</p>
-		</>
+		<div className="pageImport">
+			<h2 className="">Files to import</h2>
+			<ul className="list-disc ml-6">
+				{appData.fileObjects.map((fileObject, index) => {
+					return (
+						<li key={index}>
+							<span className="font-mono font-semibold">
+								{fileObject.pathAndFileName}
+							</span>
+							<textarea
+								value={fileObject.content}
+								readOnly
+								className="block mb-3 w-1/2 h-48 p-3 font-mono text-xs text-orange-700"
+							></textarea>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
 	);
 };
