914d8dcbf3c82e8447b0017240a171187f5db68e
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-27 10:30:29 +0100

    put in _appData

--- src/AppModel.tsx
+++ src/AppModel.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import * as appconfig from "../share/appconfig";
 import { IAppData } from "./interfaces";
@@ -5,10 +6,12 @@ import { IAppData } from "./interfaces";
 const backendUrl = `http://localhost:${appconfig.backendPort()}/appData`;
 
 export const getAppData = async () => {
+	let _appData: IAppData = {} as IAppData;
 	return new Promise<IAppData>((resolve) => {
 		(async () => {
 			const response = await axios.get(backendUrl);
-			const _appData = response.data;
+			const rawAppData:any = response.data;
+			_appData = structuredClone(rawAppData);
 			resolve(_appData);
 		})();
 	});
