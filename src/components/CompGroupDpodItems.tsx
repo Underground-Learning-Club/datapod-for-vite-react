3a4cba39880b62eb1d838b5363481f9806e29505
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-28 11:23:08 +0100

    showing base menu

--- src/components/CompGroupDpodItems.tsx
+++ src/components/CompGroupDpodItems.tsx
@@ -1,24 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import * as qstr from '../../share/qtools/qstr';
+import { useState } from "react";
+import * as qstr from "../../share/qtools/qstr";
+
+type DisplayFormatType = "table" | "json" | "csv" | "datapod";
 
 interface IProps {
 	group: any;
 }
+
 export const CompGroupDpodItems = ({ group }: IProps) => {
+	const [displayFormat, setDisplayFormat] =
+		useState<DisplayFormatType>("table");
+
 	return (
 		<section className="groupDpodItems">
-			<h3 className="text-xl mb-2">
-				{qstr.smartPlural(group.dpodItems.length, group.singleLabel)}
+			<h3 className="text-xl mb-2 flex gap-4">
+				<span>{qstr.smartPlural(group.dpodItems.length, group.singleLabel)}</span>
+				<span className="flex gap-3 text-[1rem] items-center bg-gray-300 px-4 rounded shadow-inner shadow-gray-700">
+					<p>table</p>
+					<p>JSON</p>
+				</span>
 			</h3>
 			<table>
 				<thead>
-					{group.dpodItems.map((dpodItem:any, index: number) => {
+					{group.dpodItems.map((dpodItem: any, index: number) => {
 						return (
 							<>
 								{index === 0 && (
 									<tr key={index}>
 										{dpodItem.dataTypes.map(
-											(dataType:any, index:number) => {
+											(dataType: any, index: number) => {
 												return (
 													<>
 														<th key={index}>
@@ -35,11 +46,11 @@ export const CompGroupDpodItems = ({ group }: IProps) => {
 					})}
 				</thead>
 				<tbody>
-					{group.dpodItems.map((dpodItem:any, index: number) => {
+					{group.dpodItems.map((dpodItem: any, index: number) => {
 						return (
 							<tr key={index}>
 								{dpodItem.dataTypes.map(
-									(dataType:any, index: number) => {
+									(dataType: any, index: number) => {
 										return (
 											<td key={index}>
 												{dataType.value}
