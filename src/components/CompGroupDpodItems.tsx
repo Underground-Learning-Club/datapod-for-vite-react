5090f1f88a9fa85054be06532ee61c1c8c02ac34
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-28 03:24:38 +0100

    showing 2

--- src/components/CompGroupDpodItems.tsx
+++ src/components/CompGroupDpodItems.tsx
@@ -5,25 +5,40 @@ interface IProps {
 export const CompGroupDpodItems = ({ group }: IProps) => {
 	return (
 		<>
-			<p>{group.dpodItems.length} {group.idCode}</p>
+			<p>
+				{group.dpodItems.length} {group.idCode}
+			</p>
 			<table className="groupDpodItems">
 				<thead>
-					<tr>
-						<th>111</th>
-						<th>222</th>
-					</tr>
+					{group.dpodItems.map((dpodItem, index: number) => {
+						return (
+							<tr key={index}>
+								{dpodItem.dataTypes.map((dataType, index) => {
+									return (
+										<th key={index}>
+											{dataType.label}
+										</th>
+									);
+								})}
+							</tr>
+						);
+					})}
 				</thead>
 				<tbody>
 					{group.dpodItems.map((dpodItem, index: number) => {
 						return (
 							<tr key={index}>
-								{dpodItem.dataTypes.map((dataType, index: number) => {
-									return (
-										<td key={index}>{dataType.value}</td>	
-)
-								})}
+								{dpodItem.dataTypes.map(
+									(dataType, index: number) => {
+										return (
+											<td key={index}>
+												{dataType.value}
+											</td>
+										);
+									}
+								)}
 							</tr>
-						)
+						);
 					})}
 				</tbody>
 			</table>
